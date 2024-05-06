af9985b877d8884710bf9e876a65d98434f83155
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-05-06 16:24:02 +0800

    gunakan class skill instead of asing nama and damage

--- Pokemon.java
+++ Pokemon.java
@@ -20,10 +20,12 @@ public abstract class Pokemon {
     private int XP;
     private String[] strength;
     private String[] weakness;
-    private ArrayList<String> movesNames;
-    private ArrayList<Double> movesDmgs;
-    private String[] currentMovesName;
-    private double[] currentMovesDmg;
+    //private ArrayList<String> movesNames;     x gabung move name and dmg
+    //private ArrayList<Double> movesDmgs;
+    private ArrayList<Skill> movesAndDmg; //TESTTTTTTT
+    //private String[] currentMovesName;        array nama current moves
+    //private double[] currentMovesDmg;         array dmg current moves
+    private Skill[] currentMovesAndDmg;
     
     Pokemon(String name, String type, int level, int HP, int XP){
         this.name = name;
@@ -33,20 +35,36 @@ public abstract class Pokemon {
         this.XP = XP;
         this.strength = initializeStrength();
         this.weakness = initializeWeakness();
-        this.movesNames = initializeMovesName();
-        this.movesDmgs = initializeMovesDmg();
-        this.currentMovesName = initializeCurrentMoves();
-        this.currentMovesDmg = initializeCurrentMovesDmgs();
+        //this.movesNames = initializeMovesName();      method move and dmg pokemon baru
+        //this.movesDmgs = initializeMovesDmg();
+        this.movesAndDmg = initializeMoveAndDmg(); //TESTTTTTT
+        //this.currentMovesName = initializeCurrentMoves();         initial current moves name
+        //this.currentMovesDmg = initializeCurrentMovesDmgs();      initial current moves dmg
+        this.currentMovesAndDmg = initializeCurrentMovesAndDmg();
     }
     
     public abstract String[] initializeStrength();
     
     public abstract String[] initializeWeakness();
     
-    public abstract ArrayList<String> initializeMovesName();
+    //public abstract ArrayList<String> initializeMovesName();
     
-    public abstract ArrayList<Double> initializeMovesDmg();
+    //public abstract ArrayList<Double> initializeMovesDmg();
     
+    public abstract ArrayList<Skill> initializeMoveAndDmg();
+    
+    public Skill[] initializeCurrentMovesAndDmg(){
+        Skill initialMove1, initialMove2;
+        initialMove1 = movesAndDmg.get(0);
+        initialMove2 = movesAndDmg.get(1);
+        return new Skill[]{initialMove1,initialMove2};
+    }
+    
+    public void addMoves(Skill skill){
+        movesAndDmg.add(skill);
+    }
+    
+    /*
     public String[] initializeCurrentMoves(){
         String move1, move2;
         move1 = movesNames.get(0);
@@ -60,14 +78,18 @@ public abstract class Pokemon {
         dmg2 = movesDmgs.get(1);
         return new double[]{dmg1,dmg2};
     }
+    */
     
     public int attack(int enemyHP, Pokemon myPokemon, Pokemon enemy){
         Scanner input = new Scanner(System.in);
         int move;
         int damage;
         double dmgMultiplier = 1;
+        /*
         String[] currentMoves = myPokemon.getCurrentMovesName();
         double[] currentMovesDmg = myPokemon.getCurrentMovesDmg();
+        */
+        Skill[] currentMovesAndDmg = myPokemon.getCurrentMovesAndDmg();
         String[] enemyWeakness = enemy.getWeakness();
         String[] myStrength = myPokemon.getStrength();
         for(String str1 : enemyWeakness){
@@ -79,19 +101,19 @@ public abstract class Pokemon {
         }
         System.out.println(getName()+" Moves:");
         for(int i=0; i<2; i++){
-            System.out.println(i+1+". "+currentMoves[i]);
+            System.out.println(i+1+". "+currentMovesAndDmg[i].getMoveName());
         }
         System.out.println("Which moves will "+myPokemon.getName()+" use?");
         move = input.nextInt();
         
         switch(move){
-            case 1 : System.out.println(myPokemon.getName()+" uses "+currentMoves[0]+"!");
-            damage = (int) Math.ceil(currentMovesDmg[0]*dmgMultiplier);
+            case 1 : System.out.println(myPokemon.getName()+" uses "+currentMovesAndDmg[0].getMoveName()+"!");
+            damage = (int) Math.ceil(currentMovesAndDmg[0].getDamage()*dmgMultiplier);
             enemyHP -= damage;
             break;
             
-            case 2 : System.out.println(myPokemon.getName()+" uses "+currentMoves[1]+"!");
-            damage = (int) Math.ceil(currentMovesDmg[1]*dmgMultiplier);
+            case 2 : System.out.println(myPokemon.getName()+" uses "+currentMovesAndDmg[1].getMoveName()+"!");
+            damage = (int) Math.ceil(currentMovesAndDmg[1].getDamage()*dmgMultiplier);
             enemyHP -= damage;
             break;
         }
@@ -103,8 +125,11 @@ public abstract class Pokemon {
         double dmgMultiplier = 1;
         int damage;
         int enemyMoveChoice;
+        /*
         String[] enemyMoveName = enemy.getCurrentMovesName();
         double[] enemyMoveDmg = enemy.getCurrentMovesDmg();
+        */
+        Skill[] enemyMovesAndDmg = enemy.getCurrentMovesAndDmg();
         String[] myWeakness = myPokemon.getWeakness();
         String[] enemyStrength = enemy.getStrength();
         for(String str1 : enemyStrength){
@@ -117,28 +142,33 @@ public abstract class Pokemon {
         Random rand = new Random();
         enemyMoveChoice = rand.nextInt(2)+1;
         switch(enemyMoveChoice){
-            case 1 : System.out.println(enemy.getName()+" uses "+enemyMoveName[0]+"!");
-            damage = (int) Math.ceil(enemyMoveDmg[0]*dmgMultiplier);
+            case 1 : System.out.println(enemy.getName()+" uses "+enemyMovesAndDmg[0].getMoveName()+"!");
+            damage = (int) Math.ceil(enemyMovesAndDmg[0].getDamage()*dmgMultiplier);
             myHP -= damage;
             break;
             
-            case 2 : System.out.println(enemy.getName()+" uses "+enemyMoveName[1]+"!");
-            damage = (int) Math.ceil(enemyMoveDmg[1]*dmgMultiplier);
+            case 2 : System.out.println(enemy.getName()+" uses "+enemyMovesAndDmg[1].getMoveName()+"!");
+            damage = (int) Math.ceil(enemyMovesAndDmg[1].getDamage()*dmgMultiplier);
+            myHP -= damage;
             break;
         }
         return myHP;
     }
     
     public void equipMoves(){
+        Scanner input = new Scanner(System.in);
+        int moveNum1, moveNum2;
         System.out.println("Which moves would you like to equip on"+getName());
-        for(int i=0; i<movesNames.size();i++){
-            System.out.print("\t"+i+movesNames.get(i));
+        for(int i=0; i<movesAndDmg.size();i++){
+            System.out.print("\t"+i+1+". "+movesAndDmg.get(i));
         }
-        int check = movesNames.size();
+        System.out.print("1st move: ");
+        moveNum1 = input.nextInt();
+        System.out.print("2nd move: ");
+        moveNum2 = input.nextInt();
         
-        switch(check){
-            case 1 : 
-        }
+        currentMovesAndDmg[0] = movesAndDmg.get(moveNum1-1);
+        currentMovesAndDmg[1] = movesAndDmg.get(moveNum2-1);
     }
     
     public String getName(){
@@ -169,6 +199,15 @@ public abstract class Pokemon {
         return weakness;
     }
     
+    public Skill[] getCurrentMovesAndDmg(){
+        return currentMovesAndDmg;
+    }
+    
+    public ArrayList<Skill> getMovesAndDMg(){
+        return movesAndDmg;
+    }
+    
+    /*
     public ArrayList<String> getMovesNames(){
         return movesNames;
     }
@@ -192,5 +231,6 @@ public abstract class Pokemon {
     public void addMovesDmg(double dmg){
         movesDmgs.add(dmg);
     }
+    */
     
 }
