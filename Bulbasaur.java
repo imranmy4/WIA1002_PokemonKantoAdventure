d83062b4bdd08f1e364ca5baede4f9b7724c8a2d
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-05-07 23:26:48 +0800

    bersihkan

--- Bulbasaur.java
+++ Bulbasaur.java
@@ -1,21 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pokemon.kanto.adventure;
 
 import java.util.ArrayList;
 
-
-/**
- *
- * @author IDEAPAD GAMING
- */
 public class Bulbasaur extends Pokemon{
     
     
     Bulbasaur() {
-        super("Bulbasaur","Grass/Poison",1,200,0);    
+        super("Bulbasaur","Grass/Poison",5,200,0);    
     }
 
     @Override
@@ -27,22 +19,6 @@ public class Bulbasaur extends Pokemon{
     public String[] initializeWeakness() {
        return new String[]{"Fire","Flying","Psychic"};
     }
-
-    /*@Override
-    public ArrayList<String> initializeMovesName() {
-        ArrayList<String> moveName = new ArrayList<String>();
-        moveName.add("Tackle");
-        moveName.add("Growl");
-        return moveName;
-    }
-
-    @Override
-    public ArrayList<Double> initializeMovesDmg() {
-        ArrayList<Double> moveDmg = new ArrayList<Double>();
-        moveDmg.add(20.0);
-        moveDmg.add(10.0);
-        return moveDmg;
-    }*/
 
     @Override
     public ArrayList<Skill> initializeMoveAndDmg() {
