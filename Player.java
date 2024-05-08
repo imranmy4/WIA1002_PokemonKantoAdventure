3b6dedd9646de16469fb3b609ebd997b9602b569
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-05-09 01:24:06 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,17 +2,18 @@
 package pokemon.kanto.adventure;
 
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Scanner;                                                           //Nanti tukar la kalau perlu class ni(dekat main branch github je)
 
-public class Player {
-    private ArrayList<Pokemon> pokemon = new ArrayList<Pokemon>();
-    private Pokemon currentPokemon;
+public class Player {                                                           //temporary player class
+    private ArrayList<Pokemon> pokemon = new ArrayList<Pokemon>();              //i think player pokemon can be saved in arraylist (??)
+    private Pokemon currentPokemon;                                             //pokemon currently equipped(for battle)
+    
     Player(){
-        pokemon.add(new Bulbasaur());
-        pokemon.add(new Geodude());
+        currentPokemon = starterPokemon();
+        pokemon.add(currentPokemon);
     }
     
-    public void choosePokemon(){
+    public void choosePokemon(){                                                //choose pokemon for battle
         int choice;
         Scanner input = new Scanner(System.in);
         System.out.println("Which Pokemon would you like to use?");
@@ -25,9 +26,41 @@ public class Player {
             System.out.println("\n\n");
         }
         System.out.println("Your choice: ");
+        do{
         choice = input.nextInt();
+        if(choice>pokemon.size())
+                System.out.println("Try again!");
+        } while(choice>pokemon.size());                
         currentPokemon = pokemon.get(choice-1);
-        currentPokemon.equipMoves();
+        currentPokemon.equipMoves();                                            //equip moves for pokemon
+    }
+    
+    public Pokemon starterPokemon(){
+        Scanner input = new Scanner(System.in);
+        int starter;
+        Pokemon starterPokemon = null;
+        System.out.println("Choose your starter pokemon!");
+        System.out.println("1. Bulbasaur [level 5]\n");
+        System.out.println("2. Geodude [level 5]");
+        do{
+            starter = input.nextInt();
+            switch(starter){
+                case 1 : starterPokemon = new Bulbasaur();
+                    System.out.println("You chose "+starterPokemon.getName()+"! Good choice!");
+                    System.out.println("Good luck on your journey!");
+                break;
+                
+                case 2 : starterPokemon = new Geodude();
+                    System.out.println("You chose "+starterPokemon.getName()+"! Good choice!");
+                    System.out.println("Good luck on your journey!");
+                break;
+                
+                default : System.out.println("Try again!");
+                break;
+            } 
+        }while(starterPokemon == null);
+        return starterPokemon;
+            
     }
     
     public ArrayList<Pokemon> getPokemon(){
@@ -36,5 +69,9 @@ public class Player {
     
     public Pokemon getCurrentPokemon(){
         return currentPokemon;
+    }
+    
+    public void newPokemon(Pokemon newPokemon){
+        pokemon.add(newPokemon);
     }
 }
