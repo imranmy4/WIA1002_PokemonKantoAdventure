7b60c28896d64f82b4758b62511fcc21793f80f2
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-05-11 20:30:22 +0800

    x ingat ubah apa

--- Bulbasaur.java
+++ Bulbasaur.java
@@ -6,16 +6,18 @@ import java.util.Stack;
 public class Bulbasaur extends Pokemon{
     
     Bulbasaur() {                                                           //new player bulbasaur
-        super("Bulbasaur","Grass/Poison",5,200,0);
+        super("Bulbasaur","Grass/Poison",5,100,0);
     }
     
     Bulbasaur(String location) {                                            //new wild bulbasaur
-        super("Bulbasaur","Grass/Poison",200, location);    
+        super("Bulbasaur","Grass/Poison",100, location);    
     }
     
+    /*
     Bulbasaur(int savedLevel, int savedXP){                                 //saved player bulbasaur(to be implemented)
         super("Bulbasaur", "Grass/Poison", savedLevel, 200, savedXP);
     }
+    */
 
     @Override
     public String[] setStrength() {
@@ -30,9 +32,12 @@ public class Bulbasaur extends Pokemon{
     @Override
     public Stack<Skill> allMoves() {                                    //all bulbasaur moves(top of stack is growl)
         Stack<Skill> moveset = new Stack<Skill>();
-        moveset.push(new Skill("Vine Whip",25.0,6));
-        moveset.push(new Skill("Tackle",20.0,5));
-        moveset.push(new Skill("Growl",10.0,5));
+        moveset.push(new Skill("Solar Beam",90,40));
+        moveset.push(new Skill("Power Whip",60,26));
+        moveset.push(new Skill("Razor Leaf",38.0,14));
+        moveset.push(new Skill("Vine Whip",23.0,8));
+        moveset.push(new Skill("Tackle",15.0,5));
+        moveset.push(new Skill("Growl",12.0,5));
         
         return moveset;
     }
