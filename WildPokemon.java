84696b4c6852dd9651d2df3e84e1020380d232f1
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-05-11 20:28:26 +0800

    x ingat ubah apa

--- WildPokemon.java
+++ WildPokemon.java
@@ -1,9 +1,15 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package pokemon.kanto.adventure;
 
 import java.util.ArrayList;
 
-
+/**
+ *
+ * @author IDEAPAD GAMING
+ */
 public class WildPokemon {                                          //demo wild pokemon class
     private ArrayList<Pokemon> wildPokemon;
     private String location;
