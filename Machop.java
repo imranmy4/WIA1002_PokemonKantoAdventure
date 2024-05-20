f8a55e10556fa3f2afd85c6f7b9f0a8e81058307
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-05-20 22:02:50 +0800

    Create Machop.java

---
+++ Machop.java
@@ -0,0 +1,39 @@
+
+package pokemon.kanto.adventure;
+
+import java.util.Stack;
+
+public class Machop extends Pokemon{
+
+    public Machop() {
+        super("Machop","Fighting",5,102,0);
+    }
+    
+    public Machop(String location){
+        super("Machop","Fighting",102,location);
+    }
+
+    @Override
+    public String[] setStrength() {
+        return new String[]{"Normal","Rock","Steel","Ice","Dark"};
+    }
+
+    @Override
+    public String[] setWeakness() {
+        return new String[]{"Flying","Psychic","Fairy"};
+    }
+
+    @Override
+    public Stack<Skill> allMoves() {
+        Stack<Skill> moveset = new Stack<>();
+        moveset.push(new Skill("Dynamic Punch",98.0,44));
+        moveset.push(new Skill("Seismic Toss",67.0,29));
+        moveset.push(new Skill("Knock Off",40.0,16));
+        moveset.push(new Skill("Low Sweep",24.0,9));
+        moveset.push(new Skill("Revenge",15.0,5));
+        moveset.push(new Skill("Low Kick",13.0,5));
+        
+        return moveset;
+    }
+    
+}
