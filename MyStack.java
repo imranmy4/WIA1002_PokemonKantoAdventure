581c1aaf4009029c9dbcc37159dda5a98bf3cdf3
Ahmad Iman <132155408+AhmadIman04@users.noreply.github.com>
2024-05-20 09:37:10 +0800

    ni stack aku buat sndiri

---
+++ MyStack.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kantocity;
+
+/**
+ *
+ * @author ahmad
+ */
+import java.util.ArrayList;
+public class MyStack<E>{
+    ArrayList<E> list = new ArrayList<>();
+    
+    public void push(E data){
+        list.add(data);
+    }
+    
+    public E pop(){
+        E temp = list.get(list.size()-1);
+        list.remove(list.size()-1);
+        return temp;
+    }
+    
+    public E peek(){
+        return list.get(list.size()-1);
+    }
+    
+    public int size(){
+        return list.size();
+    }
+    
+    public String toString(){
+        return list.toString();
+    }
+}
