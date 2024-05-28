555ce4305d78f664e091b14545784a949002a89f
Ahmad Iman <132155408+AhmadIman04@users.noreply.github.com>
2024-05-29 04:31:01 +0800

    Update Pikachu.java ada display method

--- Pikachu.java
+++ Pikachu.java
@@ -1,7 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemonkantoadventure;
 
-package pokemon.kanto.adventure;
-
+/**
+ *
+ * @author ahmad
+ */
 import java.util.Stack;
+import java.util.Scanner;
+import java.io.*;
 
 public class Pikachu extends Pokemon{
 
@@ -39,6 +48,18 @@ public class Pikachu extends Pokemon{
         
         
         return moveset;
+    }
+    
+    public void display(){
+        try{
+            Scanner sc = new Scanner(new FileInputStream("pikachu.txt"));
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        }catch(IOException e){
+            System.out.println("Pikachu picture is not found");
+        }
     }
     
 }
