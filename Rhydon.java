2f02d82bb3f8df04deae8355fd540761518e2133
Ahmad Iman <132155408+AhmadIman04@users.noreply.github.com>
2024-05-29 04:35:55 +0800

    Update Rhydon.java ada display function

--- Rhydon.java
+++ Rhydon.java
@@ -1,7 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemonkantoadventure;
 
-package pokemon.kanto.adventure;
-
+/**
+ *
+ * @author ahmad
+ */
 import java.util.Stack;
+import java.util.Scanner;
+import java.io.*;
 
 public class Rhydon extends Pokemon{
 
@@ -38,6 +47,18 @@ public class Rhydon extends Pokemon{
         moveset.push(new Skill("Bulldoze",18.0,5));
         
         return moveset;
+    }
+    
+    public void display(){
+        try{
+            Scanner sc = new Scanner(new FileInputStream("rhydon.txt"));
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        }catch(IOException e){
+            System.out.println("Rhydon picture is not found");
+        }
     }
     
 }
