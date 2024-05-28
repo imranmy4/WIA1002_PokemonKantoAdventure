cf0ca393aeb272649a8ebb31170ecb306418cb6c
Ahmad Iman <132155408+AhmadIman04@users.noreply.github.com>
2024-05-29 04:20:44 +0800

    Update Geodude.java ada display function

--- Geodude.java
+++ Geodude.java
@@ -1,8 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemonkantoadventure;
 
-package pokemon.kanto.adventure;
-
+/**
+ *
+ * @author ahmad
+ */
 import java.util.Stack;
-
+import java.util.Scanner ;
+import java.io.*;
 public class Geodude extends Pokemon{
 
     public Geodude() {                                                         //new player geodude
@@ -39,6 +47,18 @@ public class Geodude extends Pokemon{
         
         
         return moveset;
+    }
+    
+    public void display(){
+        try{
+            Scanner sc = new Scanner(new FileInputStream("geodude.txt"));
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        }catch(IOException e){
+            System.out.println("Geodude picture is not found");
+        }
     }
     
 }
