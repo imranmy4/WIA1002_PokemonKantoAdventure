6409c8136bd84c20a7a21041a82d6085fc474b76
Ahmad Iman <132155408+AhmadIman04@users.noreply.github.com>
2024-05-29 04:24:47 +0800

    Update MrMime.java ada display function

--- MrMime.java
+++ MrMime.java
@@ -1,8 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemonkantoadventure;
 
-package pokemon.kanto.adventure;
-
+/**
+ *
+ * @author ahmad
+ */
 import java.util.Stack;
-
+import java.util.Scanner;
+import java.io.*;
 public class MrMime extends Pokemon{
 
     public MrMime() {
@@ -38,6 +46,18 @@ public class MrMime extends Pokemon{
         moveset.push(new Skill("Pound",15.0,5));
         
         return moveset;
+    }
+    
+    public void display(){
+        try{
+            Scanner sc = new Scanner(new FileInputStream("mrmime.txt"));
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        }catch(IOException e){
+            System.out.println("Mr Mime picture is not found");
+        }
     }
     
 }
