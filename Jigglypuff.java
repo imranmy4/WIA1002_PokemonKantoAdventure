922a76996634867cc5a78145150ca7b16bd371c9
Ahmad Iman <132155408+AhmadIman04@users.noreply.github.com>
2024-05-29 04:21:46 +0800

    Update Jigglypuff.java ada display function

--- Jigglypuff.java
+++ Jigglypuff.java
@@ -1,7 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemonkantoadventure;
 
-package pokemon.kanto.adventure;
-
+/**
+ *
+ * @author ahmad
+ */
 import java.util.Stack;
+import java.util.Scanner;
+import java.io.*;
 
 public class Jigglypuff extends Pokemon{
 
@@ -35,5 +44,19 @@ public class Jigglypuff extends Pokemon{
         
         return moveset;
     }
+    
+    public void display(){
+        try{
+            Scanner sc = new Scanner(new FileInputStream("jigglypuff.txt"));
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        }catch(IOException e){
+            System.out.println("Jigglypuff picture is not found");
+        }
+    }
+    
+    
     
 }
