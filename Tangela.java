e823b3b8f9014bc8826c388d3fac23a6df26b211
Ahmad Iman <132155408+AhmadIman04@users.noreply.github.com>
2024-05-29 04:39:29 +0800

    Update Tangela.java ada display function

--- Tangela.java
+++ Tangela.java
@@ -1,7 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemonkantoadventure;
 
-package pokemon.kanto.adventure;
-
+/**
+ *
+ * @author ahmad
+ */
 import java.util.Stack;
+import java.util.Scanner;
+import java.io.*;
 
 public class Tangela extends Pokemon{
 
@@ -39,5 +48,18 @@ public class Tangela extends Pokemon{
         
         return moveset;
     }
+    public void display(){
+        try{
+            Scanner sc = new Scanner(new FileInputStream("tangela.txt"));
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        }catch(IOException e){
+            System.out.println("Tangela picture is not found");
+        }
+    }
+    
+    
     
 }
