f6e2e7b3406c04f275ae61af128d6824ceabb0cc
Ahmad Iman <132155408+AhmadIman04@users.noreply.github.com>
2024-05-29 04:25:31 +0800

    Update Muk.java ada display function

--- Muk.java
+++ Muk.java
@@ -1,7 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemonkantoadventure;
 
-package pokemon.kanto.adventure;
-
+/**
+ *
+ * @author ahmad
+ */
 import java.util.Stack;
+import java.util.Scanner;
+import java.io.*;
 
 public class Muk extends Pokemon{
 
@@ -38,6 +47,18 @@ public class Muk extends Pokemon{
         moveset.push(new Skill("Poison Gas",15.0,5));
         
         return moveset;       
+    }
+    
+    public void display(){
+        try{
+            Scanner sc = new Scanner(new FileInputStream("muk.txt"));
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        }catch(IOException e){
+            System.out.println("Muk picture is not found");
+        }
     }
     
 }
