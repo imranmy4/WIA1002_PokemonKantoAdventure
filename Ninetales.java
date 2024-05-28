9d080cb1f6d545d4b80785feef3db421379b900d
Ahmad Iman <132155408+AhmadIman04@users.noreply.github.com>
2024-05-29 04:28:29 +0800

    Update Ninetales.java ada display function

--- Ninetales.java
+++ Ninetales.java
@@ -1,7 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemonkantoadventure;
 
-package pokemon.kanto.adventure;
-
+/**
+ *
+ * @author ahmad
+ */
 import java.util.Stack;
+import java.util.Scanner;
+import java.io.*;
 
 public class Ninetales extends Pokemon{
 
@@ -38,6 +47,18 @@ public class Ninetales extends Pokemon{
         moveset.push(new Skill("Flamethrower",17.0,5));
         
         return moveset;
+    }
+    
+    public void display(){
+        try{
+            Scanner sc = new Scanner(new FileInputStream("Ninetales.txt"));
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        }catch(IOException e){
+            System.out.println("Ninetales picture is not found");
+        }
     }
     
 }
