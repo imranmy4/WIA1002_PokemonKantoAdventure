42b2be67300076bc91823a51e1295ded82a66e23
Ahmad Iman <132155408+AhmadIman04@users.noreply.github.com>
2024-05-29 04:38:06 +0800

    Update Squirtle.java ada display function

--- Squirtle.java
+++ Squirtle.java
@@ -1,7 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemonkantoadventure;
 
-package pokemon.kanto.adventure;
-
+/**
+ *
+ * @author ahmad
+ */
 import java.util.Stack;
+import java.util.Scanner;
+import java.io.*;
 
 public class Squirtle extends Pokemon{
 
@@ -40,4 +49,15 @@ public class Squirtle extends Pokemon{
         return moveset;
     }
     
+    public void display(){
+        try{
+            Scanner sc = new Scanner(new FileInputStream("squirtle.txt"));
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        }catch(IOException e){
+            System.out.println("Squirtle picture is not found");
+        }
+    }
 }
