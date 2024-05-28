408f973d48c3428f7989f24741fb9bce4bbd3935
Ahmad Iman <132155408+AhmadIman04@users.noreply.github.com>
2024-05-29 04:16:31 +0800

    Update Eevee.java ada display function

--- Eevee.java
+++ Eevee.java
@@ -1,7 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemonkantoadventure;
 
-package pokemon.kanto.adventure;
-
+/**
+ *
+ * @author ahmad
+ */
 import java.util.Stack;
+import java.util.Scanner;
+import java.io.*;
 
 public class Eevee extends Pokemon{
 
@@ -34,6 +43,18 @@ public class Eevee extends Pokemon{
         moveset.push(new Skill("Tackle",14.0,5));
         
         return moveset;
+    }
+    
+    public void display(){
+        try{
+            Scanner sc = new Scanner(new FileInputStream("eevee.txt"));
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        }catch(IOException e){
+            System.out.println("Eevee picture is not found");
+        }
     }
     
 }
