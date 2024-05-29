8c1b2bd683ce9b7bbe658ff8ef32d54851bbdae7
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-05-29 11:54:44 +0800

    handle input mismatch exception

--- KantoCity.java
+++ KantoCity.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 
 public class PokemonKantoAdventure {
