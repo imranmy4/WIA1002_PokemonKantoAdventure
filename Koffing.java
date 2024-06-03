9d938fd88319d7116d99b94244d1c1a4c6bbb224
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-06-03 17:44:41 +0800

    Update Koffing.java

--- Koffing.java
+++ Koffing.java
@@ -31,12 +31,12 @@ public class Koffing extends Pokemon {
     @Override
     public Stack<Skill> allMoves() {
         Stack<Skill> moveset = new Stack<>();
-        moveset.push(new Skill("Destiny Bond",104.0,45));
-        moveset.push(new Skill("Belch",75.0,33));
-        moveset.push(new Skill("Sludge Bomb",42.0,17,"POISONED"));
-        moveset.push(new Skill("Smog",28.0,10,"POISONED"));
-        moveset.push(new Skill("Tackle",16.0,5));
-        moveset.push(new Skill("Poison Gas",18.0,5,"POISONED"));
+        moveset.push(new Skill("Destiny Bond",104.0,41));
+        moveset.push(new Skill("Belch",75.0,29));
+        moveset.push(new Skill("Sludge Bomb",42.0,13,"POISONED"));
+        moveset.push(new Skill("Smog",28.0,6,"POISONED"));
+        moveset.push(new Skill("Tackle",16.0,1));
+        moveset.push(new Skill("Poison Gas",18.0,1,"POISONED"));
         
         return moveset;      
     }
