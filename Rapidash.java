5e96dbdc9159ce0ab781a15b4f4f21483aab2cae
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-06-03 18:43:08 +0800

    lower skill level(sekali for lower enemy level)

--- Rapidash.java
+++ Rapidash.java
@@ -31,12 +31,12 @@ public class Rapidash extends Pokemon {
     @Override
     public Stack<Skill> allMoves() {
         Stack<Skill> moveset = new Stack<>();
-        moveset.push(new Skill("Flare Blitz",107.0,49,"BURNED"));
-        moveset.push(new Skill("Fire Spin",74.0,33));
-        moveset.push(new Skill("Flame Charge",41.0,17));
-        moveset.push(new Skill("Ember",28.0,10,"BURNED"));
-        moveset.push(new Skill("Poison Jab",17.0,5));
-        moveset.push(new Skill("Megahorn",16.0,5));
+        moveset.push(new Skill("Flare Blitz",107.0,45,"BURNED"));
+        moveset.push(new Skill("Fire Spin",74.0,29));
+        moveset.push(new Skill("Flame Charge",41.0,13));
+        moveset.push(new Skill("Ember",28.0,6,"BURNED"));
+        moveset.push(new Skill("Poison Jab",17.0,1));
+        moveset.push(new Skill("Megahorn",16.0,1));
         
         return moveset;
     }
