96a19005870e041ed943b45c0c13915b3c6aff71
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 11:32:15 +0800

    Tidy Up coding style & Add on sideEffect parameter
    Tidy Up coding style:
    such as package name, import libraries order, a space before "{", a "." after the exception sout.
    
    Add on sideEffect parameter:
    "POISONED" for "Poison Jab", "Sludge Wave" moveName

--- Nidoking.java
+++ Nidoking.java
@@ -1,28 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pokemonkantoadventure;
 
-/**
- *
- * @author ahmad
- */
-import java.util.Stack;
 import java.util.Scanner;
+import java.util.Stack;
 import java.io.*;
 
-public class Nidoking extends Pokemon{
+public class Nidoking extends Pokemon {
 
     public Nidoking() {
         super("Nidoking","Poison/Ground",5,96,0);
     }
     
-    public Nidoking(String location){
+    public Nidoking(String location) {
         super("Nidoking","Poison/Ground",96,location);
     }
     
-    public Nidoking(int level){
+    public Nidoking(int level) {
         super("Nidoking","Poison/Ground",level,96);
     }
 
@@ -41,24 +33,23 @@ public class Nidoking extends Pokemon{
         Stack<Skill> moveset = new Stack<>();
         moveset.push(new Skill("Megahorn",100.0,44));
         moveset.push(new Skill("Peck",74.0,32));
-        moveset.push(new Skill("Poison Jab",51.0,21));
+        moveset.push(new Skill("Poison Jab",51.0,21,"POISONED"));
         moveset.push(new Skill("Double Kick",30.0,10));
-        moveset.push(new Skill("Sludge Wave",19.0,5));
+        moveset.push(new Skill("Sludge Wave",19.0,5,"POISONED"));
         moveset.push(new Skill("Fury Attack",18.0,5));
         
         return moveset;
     }
     
-    public void display(){
+    public void display() {
         try{
             Scanner sc = new Scanner(new FileInputStream("nidoking.txt"));
-            while(sc.hasNextLine()){
+            while(sc.hasNextLine()) {
                 System.out.println(sc.nextLine());
             }
             sc.close();
-        }catch(IOException e){
-            System.out.println("Nidoking picture is not found");
+        } catch(IOException e) {
+            System.out.println("Nidoking picture is not found.");
         }
     }
-    
 }
