877fa795500bf5e51a2f5bde4dd7fc566b084148
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 11:39:25 +0800

    Tidy Up coding style & Add on sideEffect parameter
    Tidy Up coding style:
    such as package name, import libraries order, a space before "{", a "." after the exception sout.
    
    Add on sideEffect parameter:
    "BURNED" for "Flare Blitz", "Ember" moveName

--- Rapidash.java
+++ Rapidash.java
@@ -1,28 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pokemonkantoadventure;
 
-/**
- *
- * @author ahmad
- */
-import java.util.Stack;
 import java.util.Scanner;
+import java.util.Stack;
 import java.io.*;
 
-public class Rapidash extends Pokemon{
+public class Rapidash extends Pokemon {
 
     public Rapidash() {
         super("Rapidash","Fire",5,101,0);
     }
     
-    public Rapidash(String location){
+    public Rapidash(String location) {
         super("Rapidash","Fire",101,location);
     }
     
-    public Rapidash(int level){
+    public Rapidash(int level) {
         super("Rapidash","Fire",level,101);
     }
 
@@ -39,26 +31,25 @@ public class Rapidash extends Pokemon{
     @Override
     public Stack<Skill> allMoves() {
         Stack<Skill> moveset = new Stack<>();
-        moveset.push(new Skill("Flare Blitz",107.0,49));
+        moveset.push(new Skill("Flare Blitz",107.0,49,"BURNED"));
         moveset.push(new Skill("Fire Spin",74.0,33));
         moveset.push(new Skill("Flame Charge",41.0,17));
-        moveset.push(new Skill("Ember",28.0,10));
+        moveset.push(new Skill("Ember",28.0,10,"BURNED"));
         moveset.push(new Skill("Poison Jab",17.0,5));
         moveset.push(new Skill("Megahorn",16.0,5));
         
         return moveset;
     }
     
-    public void display(){
+    public void display() {
         try{
             Scanner sc = new Scanner(new FileInputStream("rapidash.txt"));
-            while(sc.hasNextLine()){
+            while(sc.hasNextLine()) {
                 System.out.println(sc.nextLine());
             }
             sc.close();
-        }catch(IOException e){
-            System.out.println("Rapidash picture is not found");
+        } catch(IOException e) {
+            System.out.println("Rapidash picture is not found.");
         }
     }
-    
 }
