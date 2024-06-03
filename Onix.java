c0b3c761877ce7f8dd23173cbbbb9a73a5500b56
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-06-03 18:28:42 +0800

    lower skill level(sekali for lower enemy level)

--- Onix.java
+++ Onix.java
@@ -31,12 +31,12 @@ public class Onix extends Pokemon {
     @Override
     public Stack<Skill> allMoves() {
         Stack<Skill> moveset = new Stack<>();
-        moveset.push(new Skill("Stone Edge",90.0,39));
-        moveset.push(new Skill("Sand Tomb",55.0,23));
-        moveset.push(new Skill("Dragon Breath",36.0,14));
-        moveset.push(new Skill("Smack Down",24.0,9));
-        moveset.push(new Skill("Rock Throw",15.0,5));
-        moveset.push(new Skill("Bind",13.0,5));
+        moveset.push(new Skill("Stone Edge",90.0,35));
+        moveset.push(new Skill("Sand Tomb",55.0,19));
+        moveset.push(new Skill("Dragon Breath",36.0,10));
+        moveset.push(new Skill("Smack Down",24.0,5));
+        moveset.push(new Skill("Rock Throw",15.0,1));
+        moveset.push(new Skill("Bind",13.0,1));
         
         return moveset;
     }
