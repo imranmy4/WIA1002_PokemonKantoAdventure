125a1c22cda0bd5b7340733d911d55d1629ece22
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 11:37:28 +0800

    Tidy Up coding style & Add on sideEffect parameter
    Tidy Up coding style:
    such as package name, import libraries order, a space before "{", a "." after the exception sout.
    
    Add on sideEffect parameter:
    "CONFUSION" for "Water Pulse" moveName

--- Psyduck.java
+++ Psyduck.java
@@ -1,28 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pokemonkantoadventure;
 
-/**
- *
- * @author ahmad
- */
-import java.util.Stack;
 import java.util.Scanner;
+import java.util.Stack;
 import java.io.* ;
 
-public class Psyduck extends Pokemon{
+public class Psyduck extends Pokemon {
 
     public Psyduck() {
         super("Psyduck","Water",5,100,0);
     }
     
-    public Psyduck(String location){
+    public Psyduck(String location) {
         super("Psyduck","Water",100,location);
     }
     
-    public Psyduck(int level){
+    public Psyduck(int level) {
         super("Psyduck","Water",100,level);
     }
 
@@ -41,7 +33,7 @@ public class Psyduck extends Pokemon{
         Stack<Skill> moveset = new Stack<>();
         moveset.push(new Skill("Aqua Tail",107.0,43));
         moveset.push(new Skill("Zen Headbutt",72.0,29));
-        moveset.push(new Skill("Water Pulse",46.0,18));
+        moveset.push(new Skill("Water Pulse",46.0,18,"CONFUSION"));
         moveset.push(new Skill("Fury Swipes",25.0,9));
         moveset.push(new Skill("Water Gun",17.0,5));
         moveset.push(new Skill("Scratch",14.0,5));
@@ -49,16 +41,15 @@ public class Psyduck extends Pokemon{
         return moveset;
     }
     
-    public void display(){
+    public void display() {
         try{
             Scanner sc = new Scanner(new FileInputStream("psyduck.txt"));
-            while(sc.hasNextLine()){
+            while(sc.hasNextLine()) {
                 System.out.println(sc.nextLine());
             }
             sc.close();
-        }catch(IOException e){
-            System.out.println("Psyduck picture is not found");
+        } catch(IOException e) {
+            System.out.println("Psyduck picture is not found.");
         }
     }
-    
 }
