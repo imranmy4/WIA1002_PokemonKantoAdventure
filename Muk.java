9ad5c7bf3f56859a9c2fa5526cdea283fa702402
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 11:30:34 +0800

    Tidy Up coding style & Add on sideEffect parameter
    Tidy Up coding style:
    such as package name, import libraries order, a space before "{", a "." after the exception sout.
    
    Add on sideEffect parameter:
    "POISONED" for "Gunk Shot", "Sludge", "Poison Gas" moveName

--- Muk.java
+++ Muk.java
@@ -1,28 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pokemonkantoadventure;
 
-/**
- *
- * @author ahmad
- */
-import java.util.Stack;
 import java.util.Scanner;
+import java.util.Stack;
 import java.io.*;
 
-public class Muk extends Pokemon{
+public class Muk extends Pokemon {
 
     public Muk() {
         super("Muk","Poison",5,105,0);
     }
     
-    public Muk(String location){
+    public Muk(String location) {
         super("Muk","Poison",105,location);
     }
     
-    public Muk(int level){
+    public Muk(int level) {
         super("Muk","Poison",level,105);
     }
 
@@ -40,25 +32,24 @@ public class Muk extends Pokemon{
     public Stack<Skill> allMoves() {
         Stack<Skill> moveset = new Stack<>();
         moveset.push(new Skill("Memento",92.0,41));
-        moveset.push(new Skill("Gunk Shot",57.0,25));
+        moveset.push(new Skill("Gunk Shot",57.0,25,"POISONED"));
         moveset.push(new Skill("Mud Shot",39.0,16));
-        moveset.push(new Skill("Sludge",26.0,9));
+        moveset.push(new Skill("Sludge",26.0,9,"POISONED"));
         moveset.push(new Skill("Mud Slap",16.0,5));
-        moveset.push(new Skill("Poison Gas",15.0,5));
+        moveset.push(new Skill("Poison Gas",15.0,5,"POISONED"));
         
         return moveset;       
     }
     
-    public void display(){
+    public void display() {
         try{
             Scanner sc = new Scanner(new FileInputStream("muk.txt"));
-            while(sc.hasNextLine()){
+            while(sc.hasNextLine()) {
                 System.out.println(sc.nextLine());
             }
             sc.close();
-        }catch(IOException e){
-            System.out.println("Muk picture is not found");
+        } catch(IOException e) {
+            System.out.println("Muk picture is not found.");
         }
     }
-    
 }
