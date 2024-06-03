670f370f775944c87309ad63388d92a52d7dc4ae
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 11:42:15 +0800

    Tidy Up coding style & Add on sideEffect parameter
    Tidy Up coding style:
    such as package name, import libraries order, a space before "{", a "." after the exception sout.
    
    Add on sideEffect parameter:
    "POISONED" for "Spit Up" moveName

--- Victreebel.java
+++ Victreebel.java
@@ -1,28 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pokemonkantoadventure;
 
-/**
- *
- * @author ahmad
- */
-import java.util.Stack;
 import java.util.Scanner;
+import java.util.Stack;
 import java.io.*;
 
-public class Victreebel extends Pokemon{
+public class Victreebel extends Pokemon {
 
     public Victreebel() {
         super("Victreebel","Grass/Poison",5,105,0);
     }
     
-    public Victreebel(String location){
+    public Victreebel(String location) {
         super("Victreebel","Grass/Poison",105,location);
     }
     
-    public Victreebel(int level){
+    public Victreebel(int level) {
         super("Victreebel","Grass/Poison",level,105);
     }
 
@@ -42,23 +34,22 @@ public class Victreebel extends Pokemon{
         moveset.push(new Skill("Leaf Blade",85.0,37));
         moveset.push(new Skill("Leaaf Storm",57.0,25));
         moveset.push(new Skill("Power Whip",38.0,15));
-        moveset.push(new Skill("Spit Up",21.0,7));
+        moveset.push(new Skill("Spit Up",21.0,7,"POISONED"));
         moveset.push(new Skill("Razor Leaf",15.0,5));
         moveset.push(new Skill("Vine Whip",14.0,5));
         
         return moveset;
     }
     
-    public void display(){
+    public void display() {
         try{
             Scanner sc = new Scanner(new FileInputStream("Victreebel.txt"));
-            while(sc.hasNextLine()){
+            while(sc.hasNextLine()) {
                 System.out.println(sc.nextLine());
             }
             sc.close();
-        }catch(IOException e){
-            System.out.println("Victreebel picture is not found");
+        } catch(IOException e) {
+            System.out.println("Victreebel picture is not found.");
         }
     }
-    
 }
