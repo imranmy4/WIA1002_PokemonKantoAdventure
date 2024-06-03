e4fee3bce4e15f5570c313141f338de2da0638a2
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-06-03 18:32:13 +0800

    lower skill level(sekali for lower enemy level)

--- Psyduck.java
+++ Psyduck.java
@@ -31,12 +31,12 @@ public class Psyduck extends Pokemon {
     @Override
     public Stack<Skill> allMoves() {
         Stack<Skill> moveset = new Stack<>();
-        moveset.push(new Skill("Aqua Tail",107.0,43));
-        moveset.push(new Skill("Zen Headbutt",72.0,29));
-        moveset.push(new Skill("Water Pulse",46.0,18,"CONFUSION"));
-        moveset.push(new Skill("Fury Swipes",25.0,9));
-        moveset.push(new Skill("Water Gun",17.0,5));
-        moveset.push(new Skill("Scratch",14.0,5));
+        moveset.push(new Skill("Aqua Tail",107.0,39));
+        moveset.push(new Skill("Zen Headbutt",72.0,25));
+        moveset.push(new Skill("Water Pulse",46.0,14,"CONFUSION"));
+        moveset.push(new Skill("Fury Swipes",25.0,5));
+        moveset.push(new Skill("Water Gun",17.0,1));
+        moveset.push(new Skill("Scratch",14.0,1));
         
         return moveset;
     }
