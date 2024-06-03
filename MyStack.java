be85b36453ed94dc244583f51823ad13183c4bf5
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 11:31:27 +0800

    Tidy Up coding style
    Tidy Up coding style:
    such as package name, a space before "{"

--- MyStack.java
+++ MyStack.java
@@ -1,36 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package kantocity;
+package pokemonkantoadventure;
 
-/**
- *
- * @author ahmad
- */
 import java.util.ArrayList;
-public class MyStack<E>{
+
+public class MyStack<E> {
     ArrayList<E> list = new ArrayList<>();
     
-    public void push(E data){
+    public void push(E data) {
         list.add(data);
     }
     
-    public E pop(){
-        E temp = list.get(list.size()-1);
-        list.remove(list.size()-1);
+    public E pop() {
+        E temp = list.get(list.size() - 1);
+        list.remove(list.size() - 1);
         return temp;
     }
     
-    public E peek(){
-        return list.get(list.size()-1);
+    public E peek() {
+        return list.get(list.size() - 1);
     }
     
-    public int size(){
+    public int size() {
         return list.size();
     }
     
-    public String toString(){
+    public String toString() {
         return list.toString();
     }
 }
