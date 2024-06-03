833ecc332679eb3db75563a29f9349d7065a9f9a
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 11:36:33 +0800

    Tidy Up coding style & Add on sideEffect parameter
    Tidy Up coding style:
    such as package name, import libraries order, a space before "{", a "." after the exception sout.
    
    Add on sideEffect parameter:
    "PARALYZED" for "Thunder", "Thunderbolt", "Spark", "Nuzzle" moveName

--- Pikachu.java
+++ Pikachu.java
@@ -1,24 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pokemonkantoadventure;
 
-/**
- *
- * @author ahmad
- */
-import java.util.Stack;
 import java.util.Scanner;
+import java.util.Stack;
 import java.io.*;
 
-public class Pikachu extends Pokemon{
+public class Pikachu extends Pokemon {
 
     public Pikachu() {
         super("Pikachu","Electric",5,105,0);
     }
     
-    public Pikachu(String location){
+    public Pikachu(String location) {
         super("Pikachu","Electric",105, location);
     }
     
@@ -39,27 +31,25 @@ public class Pikachu extends Pokemon{
     @Override
     public Stack<Skill> allMoves() {
         Stack<Skill> moveset = new Stack<>();
-        moveset.push(new Skill("Thunder",105.0,46));
-        moveset.push(new Skill("Thunderbolt",65.0,27));
-        moveset.push(new Skill("Spark",42.0,16));
+        moveset.push(new Skill("Thunder",105.0,46,"PARALYZED"));
+        moveset.push(new Skill("Thunderbolt",65.0,27,"PARALYZED"));
+        moveset.push(new Skill("Spark",42.0,16,"PARALYZED"));
         moveset.push(new Skill("Electro Ball",23.0,8));
-        moveset.push(new Skill("Nuzzle",15.0,5));
+        moveset.push(new Skill("Nuzzle",15.0,5,"PARALYZED"));
         moveset.push(new Skill("Quick Attack",14.0,5));
-        
         
         return moveset;
     }
     
-    public void display(){
+    public void display() {
         try{
             Scanner sc = new Scanner(new FileInputStream("pikachu.txt"));
-            while(sc.hasNextLine()){
+            while(sc.hasNextLine()) {
                 System.out.println(sc.nextLine());
             }
             sc.close();
-        }catch(IOException e){
-            System.out.println("Pikachu picture is not found");
+        } catch(IOException e) {
+            System.out.println("Pikachu picture is not found.");
         }
     }
-    
 }
