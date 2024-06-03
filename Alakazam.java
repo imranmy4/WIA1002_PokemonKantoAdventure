fbd1f2ec239a43758fa3ee250b18fbf58637e89f
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-06-03 17:39:02 +0800

    lower skill level(sekali for lower enemy level)

--- Alakazam.java
+++ Alakazam.java
@@ -31,12 +31,12 @@ public class Alakazam extends Pokemon {
     @Override
     public Stack<Skill> allMoves() {
         Stack<Skill> moveset= new Stack<>();
-        moveset.push(new Skill("Future Sight",93.0,41));
-        moveset.push(new Skill("Psychic",59.0,25));
-        moveset.push(new Skill("Psyshock",37.0,15));
-        moveset.push(new Skill("Psycho Cut",26.0,9));
-        moveset.push(new Skill("Psybeam",17.0,5));
-        moveset.push(new Skill("Confusion",15.0,5,"CONFUSION"));
+        moveset.push(new Skill("Future Sight",93.0,37));
+        moveset.push(new Skill("Psychic",59.0,21));
+        moveset.push(new Skill("Psyshock",37.0,11));
+        moveset.push(new Skill("Psycho Cut",26.0,5));
+        moveset.push(new Skill("Psybeam",17.0,1));
+        moveset.push(new Skill("Confusion",15.0,1,"CONFUSION"));
         
         return moveset;
     }
