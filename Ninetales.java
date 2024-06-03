17439e64e8b0a6fbd0a9a91050180506a5ad7836
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 11:34:39 +0800

    Tidy Up coding style & Add on sideEffect parameter
    Tidy Up coding style:
    such as package name, import libraries order, a space before "{", a "." after the exception sout.
    
    Add on sideEffect parameter:
    "BURNED" for "Inferno", "Will-O-Wisp", "Ember", "Flamethrower" moveName

--- Ninetales.java
+++ Ninetales.java
@@ -1,28 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pokemonkantoadventure;
 
-/**
- *
- * @author ahmad
- */
-import java.util.Stack;
 import java.util.Scanner;
+import java.util.Stack;
 import java.io.*;
 
-public class Ninetales extends Pokemon{
+public class Ninetales extends Pokemon {
 
     public Ninetales() {
         super("Ninetales","Fire",5,105,0);
     }
     
-    public Ninetales(String location){
+    public Ninetales(String location) {
         super("Ninetales","Fire",105,location);
     }
     
-    public Ninetales(int level){
+    public Ninetales(int level) {
         super("Ninetales","Fire",level,105);
     }
 
@@ -39,26 +31,25 @@ public class Ninetales extends Pokemon{
     @Override
     public Stack<Skill> allMoves() {
         Stack<Skill> moveset = new Stack<>();
-        moveset.push(new Skill("Inferno",105.0,47));
+        moveset.push(new Skill("Inferno",105.0,47,"BURNED"));
         moveset.push(new Skill("Incinerate",80.0,35));
-        moveset.push(new Skill("Will-O-Wisp",47.0,19));
+        moveset.push(new Skill("Will-O-Wisp",47.0,19,"BURNED"));
         moveset.push(new Skill("Fire Spin",29.0,10));
-        moveset.push(new Skill("Ember",18.0,5));
-        moveset.push(new Skill("Flamethrower",17.0,5));
+        moveset.push(new Skill("Ember",18.0,5,"BURNED"));
+        moveset.push(new Skill("Flamethrower",17.0,5,"BURNED"));
         
         return moveset;
     }
     
-    public void display(){
+    public void display() {
         try{
             Scanner sc = new Scanner(new FileInputStream("Ninetales.txt"));
-            while(sc.hasNextLine()){
+            while(sc.hasNextLine()) {
                 System.out.println(sc.nextLine());
             }
             sc.close();
-        }catch(IOException e){
-            System.out.println("Ninetales picture is not found");
+        } catch(IOException e) {
+            System.out.println("Ninetales picture is not found.");
         }
     }
-    
 }
