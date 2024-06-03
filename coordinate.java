fcc5142f78ce8563a1379cfa5cc24074f6be3015
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 11:48:34 +0800

    Tidy Up coding style
    Tidy Up coding style:
    such as package name, a space before "{", a " " before and after sout.

--- coordinate.java
+++ coordinate.java
@@ -1,34 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package kantocity;
+package pokemonkantoadventure;
 
-/**
- *
- * @author ahmad
- */
 import java.util.ArrayList;
-public class coordinate{
-    int x ;
-    int y ;
-    public coordinate(int x , int y){
-        this.x = x ;
-        this.y = y ;
+
+public class coordinate {
+    int x;
+    int y;
+    
+    public coordinate(int x , int y) {
+        this.x = x;
+        this.y = y;
     }
     
-    public String toString(){
-        return "(x = "+x+", y = "+y+")";
+    public String toString() {
+        return "(x = " + x + ", y = " + y + ")";
     }
     
-    public boolean contains(ArrayList<coordinate> list){
-        for(int i=0 ; i<list.size() ; i++){
-            if(list.get(i).x==this.x && list.get(i).y==this.y){
-                return true ;
+    public boolean contains(ArrayList<coordinate> list) {
+        for(int i = 0 ; i < list.size() ; i++) {
+            if(list.get(i).x == this.x && list.get(i).y == this.y) {
+                return true;
             }
         }
         return false;
     }
-
-    
 }
