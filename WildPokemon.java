6309a59cef47646b3ff46e3247a184da50edba7b
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 11:47:15 +0800

    Tidy Up coding style
    Tidy Up coding style:
    such as package name, import libraries order, a space before "{", a " " before and after "+" in sout.

--- WildPokemon.java
+++ WildPokemon.java
@@ -1,5 +1,4 @@
-
-package pokemon.kanto.adventure;
+package pokemonkantoadventure;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,14 +10,14 @@ public class WildPokemon {                                          //demo wild 
     private String location;
     private Pokemon enemy;
     
-    WildPokemon(String location){
+    WildPokemon(String location) {
         this.location = location;
         allWildPokemon = setWildPokemon();
         wildPokemon = allWildPokemon.get(location);
         enemy = setEnemy();
     }
     
-    public HashMap<String,ArrayList<Pokemon>> setWildPokemon(){
+    public HashMap<String,ArrayList<Pokemon>> setWildPokemon() {
         ArrayList<Pokemon> PalletTown = new ArrayList<>();
         PalletTown.add(new Bulbasaur("Pallet Town"));
         PalletTown.add(new Geodude("Pallet Town"));
@@ -84,7 +83,7 @@ public class WildPokemon {                                          //demo wild 
         return temp;
     }
     
-    public Pokemon setEnemy(){
+    public Pokemon setEnemy() {
         Random rand = new Random();
         int chosen = rand.nextInt(3);
         enemy = wildPokemon.get(chosen);
@@ -92,11 +91,11 @@ public class WildPokemon {                                          //demo wild 
         return enemy;
     }
     
-    public Pokemon getEnemy(){
+    public Pokemon getEnemy() {
         return enemy;
     }
     
-    public String stringPokemonInArea(){
-        return "["+wildPokemon.get(0).getName()+","+wildPokemon.get(1).getName()+" and "+wildPokemon.get(2).getName()+" is common.]";
+    public String stringPokemonInArea() {
+        return "[" + wildPokemon.get(0).getName() + "," + wildPokemon.get(1).getName() + " and " + wildPokemon.get(2).getName() + " is common.]";
     }
 }
