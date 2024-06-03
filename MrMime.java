3a537511c00c51337c1d0311d584572e011d1b40
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 11:29:25 +0800

    Tidy Up coding style & Add on sideEffect parameter
    Tidy Up coding style:
    such as package name, import libraries order, a space before "{", a "." after the exception sout.
    
    Add on sideEffect parameter:
    "CONFUSION" for "Teeter Dance", "Psybeam", "Confusion" moveName

--- MrMime.java
+++ MrMime.java
@@ -1,27 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pokemonkantoadventure;
 
-/**
- *
- * @author ahmad
- */
-import java.util.Stack;
 import java.util.Scanner;
+import java.util.Stack;
 import java.io.*;
-public class MrMime extends Pokemon{
+
+public class MrMime extends Pokemon {
 
     public MrMime() {
         super("Mr. Mime","Psychic/Fairy",5,95,0);
     }
     
-    public MrMime(String location){
+    public MrMime(String location) {
         super("Mr. Mime","Psychic/Fairy",95,location);
     }
     
-    public MrMime(int level){
+    public MrMime(int level) {
         super("Mr. Mime","Psychic/Fairy",level,95);
     }
 
@@ -38,26 +31,25 @@ public class MrMime extends Pokemon{
     @Override
     public Stack<Skill> allMoves() {
         Stack<Skill> moveset = new Stack<>();
-        moveset.push(new Skill("Teeter Dance",97.0,43));
+        moveset.push(new Skill("Teeter Dance",97.0,43,"CONFUSION"));
         moveset.push(new Skill("Dazzling Gleam",65.0,28));
         moveset.push(new Skill("Sucker Punch",38.0,15));
-        moveset.push(new Skill("Psybeam",26.0,9));
-        moveset.push(new Skill("Confusion",14.0,5));
+        moveset.push(new Skill("Psybeam",26.0,9,"CONFUSION"));
+        moveset.push(new Skill("Confusion",14.0,5,"CONFUSION"));
         moveset.push(new Skill("Pound",15.0,5));
         
         return moveset;
     }
     
-    public void display(){
+    public void display() {
         try{
             Scanner sc = new Scanner(new FileInputStream("mrmime.txt"));
-            while(sc.hasNextLine()){
+            while(sc.hasNextLine()) {
                 System.out.println(sc.nextLine());
             }
             sc.close();
-        }catch(IOException e){
-            System.out.println("Mr Mime picture is not found");
+        } catch(IOException e) {
+            System.out.println("Mr Mime picture is not found.");
         }
     }
-    
 }
