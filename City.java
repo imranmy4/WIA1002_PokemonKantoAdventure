f5a119121b74784b8000cb3c0b9c36a79b5a8a53
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 11:22:56 +0800

    Tidy Up coding style
    Tidy Up coding style:
    such as package name, a space before "{"

--- City.java
+++ City.java
@@ -1,23 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package kantocity;
+package pokemonkantoadventure;
 
-/**
- *
- * @author ahmad
- */
 public class City {
-    String name ;
-    int index ;
+    String name;
+    int index;
     
-    public City(String name , int index){
-        this.name = name ;
-        this.index = index ;
+    public City(String name , int index) {
+        this.name = name;
+        this.index = index;
     }
     
-    public String toString(){
+    public String toString() {
         return name;
     }
 }
