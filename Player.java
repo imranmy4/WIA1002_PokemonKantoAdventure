d55915a25fce7ff52f997fff1b680a9a75a4eb60
aiminoon <149665483+aiminoon@users.noreply.github.com>
2024-06-03 12:04:14 +0800

    Tidy Up coding style & Add on some required code for Items feature
    Tidy Up coding style:
    such as package name, import libraries order, a space before "{", a " " before and after "+" in sout.
    
    Add on "antidote" variable, edited in constructor and starterPokemon method, add on getAntidote method

--- Player.java
+++ Player.java
@@ -1,33 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pokemonkantoadventure;
 
-/**
- *
- * @author ahmad
- */
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Random;
-import java.util.Scanner;                                                           //Nanti tukar la kalau perlu class ni(dekat main branch github je)
+import java.util.Scanner;                                                       
 import java.util.Stack;
 
-public class Player implements Serializable{                                                           //temporary player class
+public class Player implements Serializable {                                                      
+    int pokeball;
+    int antidote;
     private String name;
-    private ArrayList<Pokemon> pokemon = new ArrayList<>();              //i think player pokemon can be saved in arraylist (??)
-    private ArrayList<Pokemon> battlePokemon;
-    private ArrayList<Pokemon> usedPokemon = new ArrayList<>();
-    private Pokemon currentPokemon;                                             //pokemon currently equipped(for battle)
-    private int saveSlot;
     private ArrayList<String> badge = new ArrayList<>();
+    private ArrayList<Pokemon> pokemon = new ArrayList<>();              
+    private ArrayList<Pokemon> usedPokemon = new ArrayList<>();
+    private ArrayList<Pokemon> battlePokemon;
+    private Pokemon currentPokemon;                                             // pokemon currently equipped(for battle)
+    private int saveSlot;
     Stack<Pokemon> uncatchedPokemon = new Stack<>();
-    int pokeball ;
     
-    Player(String name){
+    Player(String name) {
         this.name = name;
+        this.antidote = antidote;
         pokemon.add(starterPokemon());
         currentPokemon = null;
         battlePokemon = new ArrayList<Pokemon>(pokemon);
@@ -44,17 +38,18 @@ public class Player implements Serializable{                                    
             uncatchedPokemon.push(new Geodude());
     }
     
-    public Pokemon choosePokemon(){                                                //choose pokemon for battle
-        Scanner input = new Scanner(System.in);
+    public Pokemon choosePokemon(){                                             //choose pokemon for battle
         int choice;
+        Scanner input = new Scanner(System.in);
+        
         System.out.println("Which Pokemon would you like to use?");
         System.out.println("---------------------------------------------------------------\n");
-        for(int i=0; i<battlePokemon.size();  i++){
-            System.out.print((i+1)+". "+battlePokemon.get(i).getName()+" [level "+battlePokemon.get(i).getLevel()+"]\t");
-            System.out.println("[HP : "+battlePokemon.get(i).getHP()+"/"+battlePokemon.get(i).getHP()+"]"+"\t [XP : "+battlePokemon.get(i).getXP()+"/"+battlePokemon.get(i).getXPThreshold()+"]");
+        for(int i = 0 ; i < battlePokemon.size() ; i++) {
+            System.out.print((i + 1) + ". " + battlePokemon.get(i).getName() + " [level " + battlePokemon.get(i).getLevel() + "]\t");
+            System.out.println("[HP : " + battlePokemon.get(i).getHP() + "/" + battlePokemon.get(i).getHP() + "]" + "\t [XP : " + battlePokemon.get(i).getXP() + "/" + battlePokemon.get(i).getXPThreshold() + "]");
             System.out.println("Available moves: ");
-            for(int j=0; j<battlePokemon.get(i).getMovesAndDmg().size(); j++){
-                System.out.print("\t"+(j+1)+". "+battlePokemon.get(i).getMovesAndDmg().get(j).getMoveName()+" ["+ (int) battlePokemon.get(i).getMovesAndDmg().get(j).getDamage()+" damage]");
+            for(int j = 0 ; j < battlePokemon.get(i).getMovesAndDmg().size() ; j++){
+                System.out.print("\t" + (j + 1) + ". " + battlePokemon.get(i).getMovesAndDmg().get(j).getMoveName() + " [" + (int) battlePokemon.get(i).getMovesAndDmg().get(j).getDamage() + " damage]");
             }
             System.out.println("\n");
         }
@@ -62,27 +57,27 @@ public class Player implements Serializable{                                    
         do{
             System.out.print("Your choice: ");
             choice = acceptInt();
-            if(choice>battlePokemon.size() || choice<1)
+            if(choice > battlePokemon.size() || choice < 1)
                 System.out.println("Try again!");
-        } while(choice>battlePokemon.size() || choice<1);
+        } while(choice > battlePokemon.size() || choice < 1);
         if(currentPokemon != null)
             putBackPokemon();
-        currentPokemon = battlePokemon.get(choice-1);
-        usedPokemon.add(battlePokemon.get(choice-1));
-        battlePokemon.remove(choice-1);
+        currentPokemon = battlePokemon.get(choice - 1);
+        usedPokemon.add(battlePokemon.get(choice - 1));
+        battlePokemon.remove(choice - 1);
         currentPokemon.display();
-        currentPokemon.equipMoves();//equip moves for pokemon
+        currentPokemon.equipMoves();                                            //equip moves for pokemon
         return currentPokemon;
     }    
     
-    public void resetPokemon(){
+    public void resetPokemon() {
         battlePokemon = new ArrayList<Pokemon>(pokemon);
         removeCurrentPokemon();
         usedPokemon.clear();
     }
     
-    public void updatePokemon(){
-        for(int i=0; i<usedPokemon.size(); i++){
+    public void updatePokemon() {
+        for(int i = 0 ; i < usedPokemon.size() ; i++){
             usedPokemon.get(i).resetCurrentHP();
             battlePokemon.add(usedPokemon.get(i));
         }
@@ -90,87 +85,92 @@ public class Player implements Serializable{                                    
         usedPokemon.clear();
     }
     
-    public Pokemon starterPokemon(){
+    public Pokemon starterPokemon() {
         Scanner input = new Scanner(System.in);
-        pokeball = 0 ;
+        pokeball = 0;
+        antidote = 0;
         int starter;
         Pokemon starterPokemon = null;
+        
         Bulbasaur starter1 = new Bulbasaur();
         Geodude starter2 = new Geodude();
+        
         System.out.println("---------------------------------------------------------------");
         System.out.println("Choose your starter pokemon!");
-        System.out.println("1. "+starter1.getName()+"\t["+starter1.getType()+"]\t [level "+starter1.getLevel()+"]\t [HP : "+starter1.getHP()+"/"+starter1.getHP()+"]");
+        System.out.println("1. " + starter1.getName() + "\t[" + starter1.getType() + "]\t [level " + starter1.getLevel() + "]\t [HP : " + starter1.getHP() + "/" + starter1.getHP() + "]");
         System.out.println("Moves: ");
-        for(int j=0; j<starter1.getMovesAndDmg().size(); j++){
-            System.out.print("\t"+(j+1)+". "+starter1.getMovesAndDmg().get(j).getMoveName()+" ["+ (int) starter1.getMovesAndDmg().get(j).getDamage()+" damage]");
+        for(int j = 0 ; j < starter1.getMovesAndDmg().size() ; j++){
+            System.out.print("\t" + (j + 1) + ". " + starter1.getMovesAndDmg().get(j).getMoveName() + " [" + (int) starter1.getMovesAndDmg().get(j).getDamage() + " damage]");
         }
-        System.out.println("\n\n2. "+starter2.getName()+"\t["+starter2.getType()+"]\t [level "+starter2.getLevel()+"]\t [HP : "+starter2.getHP()+"/"+starter2.getHP()+"]");
+        System.out.println("\n\n2. " + starter2.getName() + "\t[" + starter2.getType() + "]\t [level " + starter2.getLevel() + "]\t [HP : " + starter2.getHP() + "/" + starter2.getHP() + "]");
         System.out.println("Moves: ");
-        for(int j=0; j<starter2.getMovesAndDmg().size(); j++){
-            System.out.print("\t"+(j+1)+". "+starter2.getMovesAndDmg().get(j).getMoveName()+" ["+ (int) starter2.getMovesAndDmg().get(j).getDamage()+" damage]");
+        for(int j = 0 ; j < starter2.getMovesAndDmg().size() ; j++){
+            System.out.print("\t" + (j + 1) + ". " + starter2.getMovesAndDmg().get(j).getMoveName() + " [" + (int) starter2.getMovesAndDmg().get(j).getDamage() + " damage]");
         }
         System.out.println();
         System.out.println("---------------------------------------------------------------\n");
         do{
             starter = acceptInt();
-            switch(starter){
-                case 1 : starterPokemon = starter1;
+            switch(starter) {
+                case 1 : 
+                    starterPokemon = starter1;
                     starter1.display();
-                    System.out.println("You chose "+starterPokemon.getName()+"! Good choice!");
+                    System.out.println("You chose " + starterPokemon.getName() + "! Good choice!");
                     System.out.println("Good luck on your journey!");
-                break;
+                    break;
                 
-                case 2 : starterPokemon = starter2;
+                case 2 : 
+                    starterPokemon = starter2;
                     starter2.display();
-                    System.out.println("You chose "+starterPokemon.getName()+"! Good choice!");
+                    System.out.println("You chose " + starterPokemon.getName() + "! Good choice!");
                     System.out.println("Good luck on your journey!");
-                break;
+                    break;
                 
-                default : System.out.println("Try again!");
-                break;
+                default : 
+                    System.out.println("Try again!");
+                    break;
             }
             System.out.println("---------------------------------------------------------------");
-        }while(starterPokemon == null);
+        } while(starterPokemon == null);
         
         return starterPokemon;
-            
     }
     
-    public void catchPokemon(){
+    public void catchPokemon() {
         Random rand = new Random();
         Scanner input = new Scanner(System.in);
         String choice;
         
         if(uncatchedPokemon.isEmpty())
             return;
-        else{
-            int chance = (rand.nextInt(7)+1);
-            switch(chance){
+        else {
+            int chance = (rand.nextInt(7) + 1);
+            switch(chance) {
                 case 1:
                     System.out.println("---------------------------------------------------------------");
-                    System.out.println("You encountered a wild "+uncatchedPokemon.peek().getName()+"!");
-                    System.out.println("You have "+ pokeball+ " pokeballs ");
+                    System.out.println("You encountered a wild " + uncatchedPokemon.peek().getName() + "!");
+                    System.out.println("You have " + pokeball + " pokeballs ");
                     System.out.print("Would you like to catch it? [yes/no] : ");
                     do{
                     choice = input.nextLine();
-                    if(choice.equalsIgnoreCase("yes")){
+                    if(choice.equalsIgnoreCase("yes")) {
                         pokeball--;
-                        int pokeball_probability=rand.nextInt(2);
-                        switch(pokeball_probability){
+                        int pokeball_probability = rand.nextInt(2);
+                        switch(pokeball_probability) {
                         case 1:
                             System.out.println("Oops, the pokemon got away, better luck next time .");
                             break;
                         case 0:
-                            System.out.println("You have obtained "+uncatchedPokemon.peek().getName()+"!");
+                            System.out.println("You have obtained " + uncatchedPokemon.peek().getName() + "!");
                             newPokemon(uncatchedPokemon.pop());
                             break;
                         }
-                        System.out.println("You now have "+pokeball+" pokeballs ");
+                        System.out.println("You now have " + pokeball + " pokeballs ");
                     }
                     else if(choice.equalsIgnoreCase("no"))
                         System.out.println("You'll have another chance to catch it next time!");
                     else
-                            System.out.print("Invalid input! [yes/no] : ");
+                        System.out.print("Invalid input! [yes/no] : ");
                     } while(!(choice.equalsIgnoreCase("yes") || choice.equalsIgnoreCase("no")));
                     System.out.println("---------------------------------------------------------------");
                     break;
@@ -180,70 +180,73 @@ public class Player implements Serializable{                                    
         }
     }
     
-    public void removeCurrentPokemon(){
+    public void removeCurrentPokemon() {
         currentPokemon = null;
     }
     
-    public void putBackPokemon(){
+    public void putBackPokemon() {
         battlePokemon.add(currentPokemon);
         currentPokemon = null;
     }
     
-    public ArrayList<Pokemon> getPokemon(){
+    public ArrayList<Pokemon> getPokemon() {
         return pokemon;
     }
     
-    public ArrayList<Pokemon> getBattlePokemon(){
+    public ArrayList<Pokemon> getBattlePokemon() {
         return battlePokemon;
     }
     
-    public ArrayList<Pokemon> getUsedPokemon(){
+    public ArrayList<Pokemon> getUsedPokemon() {
         return usedPokemon;
     }
     
-    public Pokemon getCurrentPokemon(){
+    public Pokemon getCurrentPokemon() {
         return currentPokemon;
     }
     
-    public void newPokemon(Pokemon newPokemon){
+    public void newPokemon(Pokemon newPokemon) {
         pokemon.add(newPokemon);
         battlePokemon = new ArrayList<Pokemon>(pokemon);
     }
     
-    public String getName(){
+    public String getName() {
         return name;
     }
     
-    public void setSaveSlot(int saveSlot){
+    public void setSaveSlot(int saveSlot) {
         this.saveSlot = saveSlot;
     }
     
-    public int getSaveSlot(){
+    public int getSaveSlot() {
         return saveSlot;
     }
     
-    public boolean battlePokemonEmpty(){
+    public boolean battlePokemonEmpty() {
         return battlePokemon.isEmpty();
     }
     
-    public ArrayList<String> getBadges(){
+    public ArrayList<String> getBadges() {
         return badge;
     }
     
-    public void obtainBadge(String badge){
+    public void obtainBadge(String badge) {
         this.badge.add(badge);
     }
 
-    public static int acceptInt(){
+    public static int acceptInt() {
         int i;
         Scanner input = new Scanner(System.in);
         try{
             i = input.nextInt();
             input.nextLine();
             return i;
-        } catch(InputMismatchException ex){
+        } catch(InputMismatchException ex) {
             return -1;
         }
-        
+    }
+    
+    public int getAntidote() {
+        return antidote;
     }
 }
