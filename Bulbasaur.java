87557b5e8d5bbedda93e9f37f4b7708c71494e8e
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-06-04 12:15:03 +0800

    Update Bulbasaur.java

--- Bulbasaur.java
+++ Bulbasaur.java
@@ -5,34 +5,37 @@ import java.util.Stack;
 
 public class Bulbasaur extends Pokemon{
     
-    Bulbasaur() {                                                           //new player bulbasaur
-        super("Bulbasaur","Grass/Poison",5,200,0);
+    public Bulbasaur() {                                                           //new player bulbasaur
+        super("Bulbasaur","Grass/Poison",5,100,0);
     }
     
-    Bulbasaur(String location) {                                            //new wild bulbasaur
-        super("Bulbasaur","Grass/Poison",200, location);    
+    public Bulbasaur(String location) {                                            //new wild bulbasaur
+        super("Bulbasaur","Grass/Poison",100, location);    
     }
     
-    Bulbasaur(int savedLevel, int savedXP){                                 //saved player bulbasaur(to be implemented)
-        super("Bulbasaur", "Grass/Poison", savedLevel, 200, savedXP);
+    public Bulbasaur(int level) {                                                           //gym leader pokemon
+        super("Bulbasaur","Grass/Poison",level,100);
     }
-
+    
     @Override
     public String[] setStrength() {
-        return new String[]{"Water","Ground","Rock"};
+        return new String[]{"Water","Ground","Rock","Fairy"};
     }
 
     @Override
     public String[] setWeakness() {
-       return new String[]{"Fire","Flying","Psychic"};
+       return new String[]{"Fire","Flying","Psychic","Ice"};
     }
 
     @Override
     public Stack<Skill> allMoves() {                                    //all bulbasaur moves(top of stack is growl)
         Stack<Skill> moveset = new Stack<Skill>();
-        moveset.push(new Skill("Vine Whip",25.0,6));
-        moveset.push(new Skill("Tackle",20.0,5));
-        moveset.push(new Skill("Growl",10.0,5));
+        moveset.push(new Skill("Solar Beam",90,36));
+        moveset.push(new Skill("Power Whip",60,22));
+        moveset.push(new Skill("Razor Leaf",38.0,10));
+        moveset.push(new Skill("Vine Whip",23.0,4));
+        moveset.push(new Skill("Tackle",15.0,1));
+        moveset.push(new Skill("Growl",13.0,1));
         
         return moveset;
     }
