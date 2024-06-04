f6df68eb6179bd62a919d1c073964992d2c1d48c
imranmy4 <149666023+imranmy4@users.noreply.github.com>
2024-06-04 12:20:20 +0800

    Update Skill.java

--- Skill.java
+++ Skill.java
@@ -1,23 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pokemon.kanto.adventure;
 
-/**
- *
- * @author IDEAPAD GAMING
- */
-public class Skill {                                            //class for pokemon moves
+import java.io.Serializable;
+
+public class Skill implements Serializable{                                            //class for pokemon moves
     private String moveName;
     private double damage;
     private int level;
+    private String sideEffect;
+    private static final double SIDE_EFFECT_CHANCE = 0.4;
     
     Skill(String moveName, double damage, int level){
         this.moveName = moveName;
         this.damage = damage;
         this.level = level;
     }
+    
+    Skill(String moveName, double damage, int level, String sideEffect) {
+        this.moveName = moveName;
+        this.damage = damage;
+        this.level = level;
+        this.sideEffect = sideEffect;
+    }
+    
     public String getMoveName() {
         return moveName;
     }
